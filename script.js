0069cb450e8c59e5b0c52150e723a1d4b14b6cbe
chaithanya09-0 <nnm23cs048@nmamit.in>
2025-07-02 21:13:42 +0530

    a function to get the computer choice by random has been added

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function getComputerChoice (){
+    let num = Math.floor( Math.random() * 10 )
+    console.log(num)
+    num = num % 3
+    console.log(num)
+    return (num === 0) ? "rock" :
+    (num === 1) ? "paper" : "scissors"
+}
+let computerChoice=getComputerChoice()
+console.log(computerChoice)
