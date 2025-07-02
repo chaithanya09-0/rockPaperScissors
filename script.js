b7cce2347f9763bc5dbf7b9e689536480184e38f
chaithanya09-0 <nnm23cs048@nmamit.in>
2025-07-02 22:23:18 +0530

    final changes are made and updated
    The game is played for 5 rounds and scores at each round is counted seperately and winner is declared at the end.

--- script.js
+++ script.js
@@ -1,18 +1,74 @@
+let humanScore = 0
+let computerScore = 0
 
 function getComputerChoice (){
     let num = Math.floor( Math.random() * 10 )
-    console.log(num)
+    
     num = num % 3
-    console.log(num)
+    
     return (num === 0) ? "rock" :
     (num === 1) ? "paper" : "scissors"
 }
-let computerChoice=getComputerChoice()
-console.log(computerChoice)
+let computerChoice;
+
 
 function getHumanChoice() {
     userChoice = prompt ("Enter your choice[rock/paper/scissors]")
     userChoice = userChoice.toLowerCase()
     return userChoice
 }
-getHumanChoice()
+let humanChoice;
+
+function playRound(humanChoice , computerChoice) {
+    if (humanChoice === "rock"){
+        if (computerChoice === "rock") {
+            console.log("Computer chose rock")
+            console.log("tie! no points for both players")
+        } else if (computerChoice === "paper") { 
+            console.log("Computer chose paper")
+            console.log("You lose!");
+            computerScore++;
+        } else { 
+            console.log("Computer chose scissors")
+            console.log("You Win!")
+            humanScore++ 
+        }
+    }
+    if (humanChoice === "paper"){
+        if (computerChoice === "rock") {
+            console.log("Computer chose rock")
+            console.log("You win!")
+            humanScore++
+        } else if (computerChoice === "paper") { 
+            console.log("Computer chose paper")
+            console.log("tie! no points for both players");
+        } else { 
+            console.log("Computer chose scissors")
+            console.log("You lose!")
+            computerScore++ 
+        }
+    }
+    if (humanChoice === "scissors"){
+        if (computerChoice === "rock") {
+            console.log("Computer chose rock")
+            console.log("You lose!");
+            computerScore++;
+        } else if (computerChoice === "paper") { 
+            console.log("Computer chose paper")
+            console.log("You Win!")
+            humanScore++ 
+        } else { 
+            console.log("Computer chose scissors")
+            console.log("tie! no points for both players");
+        }
+    }
+}
+function playGame(){
+    for (let i=0; i <5; i++){
+        computerChoice = getComputerChoice()
+        humanChoice = getHumanChoice()
+        playRound(humanChoice , computerChoice)
+    }
+    (computerScore > humanScore) ? console.log("Computer won the game") : console.log("You won the game")
+}
+playGame();
